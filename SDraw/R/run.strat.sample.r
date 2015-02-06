0283b978e6f214ba1a9c346cd1c4cefca04e640d
gtd80526 <gdidonato@west-inc.com>
2015-02-06 13:38:22 -0700

    First Stratified Commit
    this is for 3 functions. the first adds options to the SDraw menu, and
    the other 2 set up a stratified GUI and process those inputs. Not
    Working

---
+++ SDraw/R/run.strat.sample.r
@@ -0,0 +1,90 @@
+run.strat.sample <- function(button, dat){
+#tester for running stratified sample
+
+    #   Query the entry fields
+    n <- dat$n.entry$getText()
+    fn <- dat$shape.in.entry$getText()
+	strat.var <- dat$strata.var.entry$getText()
+    outobj <- dat$out.r.entry$getText()
+    over.n <- dat$over.entry$getText()
+    seed <- dat$seed.entry$getText()
+    stype <- dat$samp.type.combo$getActiveText()
+    stype <- substring(stype, 1, 4)
+    n.strata <- dat$user.samp.entry$getText() #vector of sample sizes for strata
+	
+    #   Set seed if there is a number present
+    if( nchar(seed) > 0 ){
+        seed <- as.numeric( seed )
+        set.seed( seed )
+    }
+
+   #   Get sample allocation information from radio buttons
+   if( dat$prop.rb$getActive() ){
+       alloc.type <- "proportional"
+   } else if ( dat$const.rb$getActive() ){
+       alloc.type <- "constant"
+   } else {
+       alloc.type <- "user"
+   }
+   ## will need a function call here to use sample allocation info to determine sample sizes
+   ## first step is to get it to read a user-input vector of strata sample sizes
+   
+   # actually, this can be generalized to a function that determines the sample sizes of the strata,
+   # regardless of whether they are proportional, constant, or user-defined
+   #
+   # strata.sizes <- function(alloc.type, n.strata=NULL)
+   # 
+   #}
+    # #   Check input parameters
+    if( length(n) == 0 | (n <= 0) ){
+        error.message("Sample size not specified or is less than 0.")
+        return()
+    }
+
+    if( nchar(fn) == 0 | (fn == " ") ){
+        error.message("Input shape file not specified.")
+        return()
+    }
+
+    if( exists( outobj, where=.GlobalEnv ) ){
+        #   Ideally, we could ask the user here if they want to overwrite.
+        #   This should be easy using RGtk windows, but I am in a rush.
+        #   For now, just save a backup copy
+        assign( paste(outobj, ".previous", sep=""), get(outobj, pos=.GlobalEnv), pos=.GlobalEnv)
+        cat( paste( "Old version of", outobj, "copied to", paste(outobj, ".previous", sep=""), "\n"))
+    }
+
+
+    #   fix up the sample sizes
+    # n <- as.numeric(as.vector( n ))
+	# over.n <- as.numeric(as.vector(over.n))
+    if( nchar(over.n) == 0 ){ #right now this just works on 1 oversample, which is fine
+        over.n <- 0
+    } else {
+        over.n <- as.numeric( over.n ) #whatever the over.n is, we can use that in each strata
+    }
+    if( is.na(over.n) ){
+        warning("Oversample set to 0.")
+        over.n <- 0
+    }
+
+    #   Actually draw the sample
+    #   Remember that fn is the text string name of the shapefile, without .shp, and without path.
+    samp <- switch( stype, 
+                #"BAS " = draw.bas(n,over.n,fn),
+                "GRTS" = draw.strat.grts(n,over.n,fn,strat.var), #we need a new function for this, same inputs but adds name of stratifying variable
+                #"SSS " = draw.sss(n,over.n,fn),
+                         stop(paste("Unknown sample type:",stype)))
+
+    #   Save the sample in global environment.  Type of sample is an attribute.                         
+    assign( outobj, samp, pos=.GlobalEnv )
+
+    #   Tell user we are finished.
+    cat("First 10 sample locations:\n")
+    print(samp[1:10,])
+
+    dialog <- gtkMessageDialogNew(NULL, c("modal"), "info", "ok", stype, "draw successful.")
+    dialog$run()
+    dialog$destroy()
+	
+}
