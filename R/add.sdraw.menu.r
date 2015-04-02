d938900a6c46605eaad8a6da05560c9fa2fa8ee4
tmcd <tmcdonald@west-inc.com>
2015-04-02 13:48:33 -0600

    Revert "Backward changes to add.sdraw.menu"
    This reverts commit 60c75cf6dd65ea70f3bbd7c7370072c784ea0600.

--- R/add.sdraw.menu.r
+++ R/add.sdraw.menu.r
@@ -9,14 +9,19 @@ if( sys == "Windows" & interactive() & !rstudio){
     winMenuAdd("SDraw")
 	winMenuAddItem("SDraw/Sample Draws", "Equi-Probable...", "equi.GUI()")
 	winMenuAddItem("SDraw/Sample Draws", "Stratified...", "stratified.GUI()")
-	#winMenuAddItem("SDraw/Sample Draws", "Variable ProbableAdvanced ...", "advanced.GUI()")
+	#winMenuAddItem("SDraw/Sample Draws", "Variable Probable...", "advanced.GUI()")
 	winMenuAddItem("SDraw/Analysis", "CDF", "none")
-} else {
+} else if(!rstudio){
     cat("This is a non-menu environment.\n")
     cat("Call one of the following functions:\n")
     cat("\t equi.GUI() -> Interface to draw equi-probable samples.\n")
 	cat("\t stratified.GIU() -> Interface to draw stratified samples.\n") #added by Guy, 1/2/15
     #cat("\t advanced.GUI() -> Interface to draw advanced samples.\n")
+} else {
+  winDialog( "ok", paste("You are running in RStudio where an SDraw menu cannot be created.", " ",
+                         "Execute one of the following functions in the Console window:",
+                         "  equi.GUI() -> Interface to draw equi-probable samples.",
+                         "  stratified.GIU() -> Interface to draw stratified samples.", sep="\n"))
 }
 
 }
