58f0c854574b2b6d856f1dd2216759d1e7160e57
tmcd <tmcdonald@west-inc.com>
2015-04-14 08:50:39 -0600

    Commit to address errors in R -check

--- R/add.sdraw.menu.r
+++ R/add.sdraw.menu.r
@@ -5,23 +5,24 @@ add.sdraw.menu <- function(){
 
 sys <-  Sys.info()["sysname"]
 rstudio <-  Sys.getenv("RSTUDIO") == "1"
+
 if( sys == "Windows" & interactive() & !rstudio){
-    winMenuAdd("SDraw")
+  winMenuAdd("SDraw")
 	winMenuAddItem("SDraw/Sample Draws", "Equi-Probable...", "equi.GUI()")
 	winMenuAddItem("SDraw/Sample Draws", "Stratified...", "stratified.GUI()")
 	#winMenuAddItem("SDraw/Sample Draws", "Variable Probable...", "advanced.GUI()")
 	winMenuAddItem("SDraw/Analysis", "CDF", "none")
 } else if(!rstudio){
-    cat("This is a non-menu environment.\n")
-    cat("Call one of the following functions:\n")
-    cat("\t equi.GUI() -> Interface to draw equi-probable samples.\n")
+  cat("This is a non-menu environment.\n")
+  cat("Call one of the following functions:\n")
+  cat("\t equi.GUI() -> Interface to draw equi-probable samples.\n")
 	cat("\t stratified.GIU() -> Interface to draw stratified samples.\n") #added by Guy, 1/2/15
-    #cat("\t advanced.GUI() -> Interface to draw advanced samples.\n")
+  #cat("\t advanced.GUI() -> Interface to draw advanced samples.\n")
 } else {
-  winDialog( "ok", paste("You are running in RStudio where an SDraw menu cannot be created.", " ",
-                         "To see the dialogs, execute one of the following in the Console window:",
-                         "  equi.GUI() -> Interface to draw equi-probable samples.",
-                         "  stratified.GIU() -> Interface to draw stratified samples.", sep="\n"))
+  cat("You are running in RStudio where an SDraw menu cannot be created.\n")
+  cat("To see the dialogs, execute one of the following in the Console window:\n")
+  cat("  equi.GUI() -> Interface to draw equi-probable samples.\n")
+  cat("  stratified.GIU() -> Interface to draw stratified samples.\n")
 }
 
 }
