f3436e19e0cb097a5ecec2b08c428fcacf5f315c
tmcd <tmcdonald@west-inc.com>
2015-07-23 16:03:20 -0600

    Deleted large data and Doc directories

--- R/zzz.r
+++ R/zzz.r
@@ -8,17 +8,5 @@
 #   packageStartupMessage("\nWEST Inc. (tmcdonald@west-inc.com)") 
 
 
-#    cat("\nSee docs/GTK2_Runtime_install_instructions.pdf for help installing the GTK+\n")
-#    cat("runtime library required for R vesion >= 2.11.0 on Windows machines.\n")
-
-    add.sdraw.menu()
-    
-#    SDrawNamespaceEnv = asNamespace( "SDraw" )
-#    SDrawPackageSpace <- as.environment( "package:SDraw" )
-#    print(ls(env=SDrawNamespaceEnv, all=T)) 
-#    print(ls(env=SDrawPackageSpace, all=T)) 
-
-#    assign(".INPUT.DIR", getwd(), envir=.GlobalEnv )
-#    print(ls(env=.GlobalEnv, all=T))
 
 }
