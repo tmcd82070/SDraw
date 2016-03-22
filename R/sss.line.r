5460d68626bd46b1abb8257e532b559ccaa35e93
Trent McDonald <tmcdonald@west-inc.com>
2016-03-21 23:06:27 -0600

    None working commit of sss.lint

--- R/sss.line.r
+++ R/sss.line.r
@@ -43,21 +43,75 @@
 #' points( HI.samp, col="red", pch=16 )
 #' 
 #' 
-sss.line <- function(x, n, spacing=NA, random.start=TRUE){
+sss.line <- function(x, n, spacing, random.start=TRUE){
 
-if( !(inherits(x, "SpatialLines")) ) stop("Must call sss.line with a SpatialLines* object.")
+  if( !(inherits(x, "SpatialLines")) ) stop("Must call sss.line with a SpatialLines* object.")
 
-#   This is easy, use spsample from the sp package.
-#   Note: this is the only function that requires the Depends: sp statement in DESCRIPTION.
-#   Without this call to spsample, you can Import sp, rather than Depend it.
-samp <- spsample( x, n, type="regular" )
+  if(!missing(x) & !missing(spacing)) stop("Must specify only one of n or spacing in sss.line")
 
+  # Internal function to sample a single line -----
+  sss.single.line <- function(coords){
+    # keep in mind coords is a list that could be of length 2+
+    offset = offset[1]
+    lapply(coords, function(cc){
+      lens = LineLength(cc, longlat = FALSE, sum = FALSE)
+      if (any(abs(lens) < .Machine$double.eps)) {
+        wl <- which(abs(lens) < .Machine$double.eps)
+        cc <- cc[-(wl), ]
+        lens <- lens[-(wl)]
+      }
+      csl = c(0, cumsum(lens))
+      maxl = csl[length(csl)]
+      pts = ((1:n) - (1 - offset))/n * maxl  
+      here~~~~~!!!
+      int = findInterval(pts, csl, all.inside = TRUE)
+      where = (pts - csl[int])/diff(csl)[int]
+      xy = cc[int, , drop = FALSE] + where * (cc[int + 1, , drop = FALSE] - 
+                                              cc[int, , drop = FALSE])
+    if (nrow(xy) < 1) 
+      return(NULL)
+    SpatialPoints(xy, proj4string)
+  }
+
+  # Rest of sss.line ------
+  
+  # check input parameters
+  if(missing(n)){
+    # spacing has been specified
+    n <- sum(lens) / spacing
+  } 
+
+  if (!is.finite(n) || n < 1) return(NULL)
+
+  # keep in mind that x@lines[[1]] could be 2+ Lines
+  L = x@lines
+  lens = sapply(L, LinesLength)
+  if (sum(lens) < .Machine$double.eps) stop("Lines object has no length")
+
+  # Number of points to put on each line
+  nrs = round(lens/sum(lens) * n)
+  
+  # allocate list to hold points
+  ret = vector("list", sum(nrs > 0))
+  
+  j <- 1
+  for (i in 1:length(L)) {
+    if (nrs[i] > 0) {
+      ret[[j]] = sss.single.line(coordinates(L[[i]]), nrs[i])
+      j <- j+1
+    }
+  }
+
+  ret <- do.call(rbind, ret)  
+
+
+# old code:
 #   Make answer into a SpatialPointsDataFrame to be consistent with other SDraw routines
 #   It would be nice to transfer over the attributes of x, but x is a line, 
 #   and the over() function does not take points over lines (makes sense, no area to lines). 
 #   And, I cannot figure out a nice way to buffer the lines and use over.
 
-samp <- SpatialPointsDataFrame( samp,  data=data.frame(siteID=1:length(samp)) )
+#samp <- SpatialPointsDataFrame( samp,  data=data.frame(siteID=1:length(samp)) )
 
 samp
 
