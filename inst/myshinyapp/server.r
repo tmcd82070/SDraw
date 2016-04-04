75b862665b3dae4f90468d102a78358efc08b711
mandykauffman <mkauffman@west-inc.com>
2016-04-04 10:01:59 -0600

    server/ui updates
    A handful of modifications.  (1) Changes to UI layout -- this still
    needs work.  (2) Changes to plotting so that sampled points will show up
    on top of available points.  (3) Changes to server code forcing order of
    operations (i.e. do NOT draw sample until run button is pressed).  (4)
    Slight modifications to file export.  Please not that file export DOES
    NOT work through RStudio interface - you need to run the app in the
    browser to use this functionality.  (5) shinydashboard package is used
    here so we will need to add to the DESCRIPTION file if we want to keep
    using it.

--- inst/myshinyapp/server.r
+++ inst/myshinyapp/server.r
@@ -1,11 +1,12 @@
 server <- function(session,input, output){
   
   require(rgdal)
+  require(shinydashboard)
   
   #increase file upload size to 30MB
   options(shiny.maxRequestSize=30*1024^2)
   
-  #display the shapefile image when uploaded
+  #process shapefile inputs
   shape <- reactive({
     myshape<- input$shape
     if (is.null(myshape)) 
@@ -17,67 +18,84 @@ server <- function(session,input, output){
     getshp <- list.files(dir, pattern="*.shp", full.names=FALSE)
     getshp <- getshp[grep('.xml',getshp,invert= TRUE)]
     shape<-rgdal::readOGR(dir, gsub('.shp','',getshp), verbose = FALSE)
-    })
+  })
   
-   
+  # show shapefile (& sample if produced)
   output$shape<-renderPlot({
-    if(is.null(input$shape))
-      return(NULL)
-    plot(shape())
+    if(is.null(input$shape)){
+     return(NULL)
+    } else {  
+     plot(shape())
+    }
+    if(input$Run){
+     points(run(), col = 'red')
+    }
   })
  # end plot of original shapefile 
   
   
 
    # 'run' function recognizes the shapefile as points, lines, or polygons  
-  run <- reactive({
-    #make text outside here
-    text=paste(paste(tolower(input$method),tolower(substr(as.character(gsub('Spatial|DataFrame','',class(shape()))), 1, nchar(as.character(gsub('Spatial|DataFrame','',class(shape()))))-1)),sep='.'), '(', 'n =', input$n, ',', 'shp = shape()',')')
+   run <- reactive({
+    
+    text=paste(paste(tolower(input$method),tolower(substr(as.character(gsub('Spatial|DataFrame','',class(shape()))), 
+                                                          1, nchar(as.character(gsub('Spatial|DataFrame','',
+                                                                                     class(shape()))))-1)),sep='.'),
+               '(', 'n =', input$n, ',', 'shp = shape()',')')
+    
     isolate({
-       eval(parse(text=text))
-    })})
+      withProgress(message = 'Generating sample...',
+                   value = 0.75,
+                   {
+      out = eval(parse(text=text))
+      return(out)
+      setProgress(1)
+                   }) # end progress bar
+    }) # end isolate 
+    
+  }) # end reactive
   
-
-  
-  # for run button, execute above 'run' function for selected input$method (HAL, BAS, GRTS, SSS)
-  #observeEvent(input$Run, {run(paste(tolower(input$method, '.', text, sep = "")))}) 
-  output$shape2<-renderPlot({
-    if(!input$Run)
-      return(NULL)
-      plot(run())
+  # Only take sample when run button has been pressed
+  observe({
+    go <- input$Run
+    if(go){
+      run()
+    }
   })
   
   # determine what the 'run' function is doing (what function is it calling)
-  output$text<-renderText({
-    if(!input$Run)
-      return(NULL)
-      paste(text=paste(paste(tolower(input$method),tolower(substr(as.character(gsub('Spatial|DataFrame','',class(shape()))), 1, nchar(as.character(gsub('Spatial|DataFrame','',class(shape()))))-1)),sep='.')))
-    })
-  
+  # output$text<-renderText({
+  #   if(!input$Run)
+  #     return(NULL)
+  #     paste(text=paste(paste(tolower(input$method),tolower(substr(as.character(gsub('Spatial|DataFrame','',class(shape()))), 1, nchar(as.character(gsub('Spatial|DataFrame','',class(shape()))))-1)),sep='.')))
+  #   })
+  # 
 
   filename = paste('data-', gsub('-| |\\:','',Sys.time()),sep='')
+  
   #for export button
   #currently handles .csv and .shp file types - not working
   output$Export<- downloadHandler(
     filename = filename,
     content = function(file){
       if(input$outputType == 'CSV'){
-        write.csv(shape2()@data, paste0(choose.dir(),'\\',filename,'.csv'))
+        write.csv(run()@data, file=paste0(choose.dir(),'\\',filename,'.csv'))
       } 
-      if(input$outputType == 'SHP'){
-        writeOGR(obj=shape2(),
+      #if(input$outputType == 'SHP'){
+      if(input$outputType != 'CSV'){
+        writeOGR(obj=run(),
                  #dsn='c:/users/mkauffman/downloads',
                  dsn = choose.dir(),
                  layer=filename,
-                 driver='ESRI Shapefile')
-                 #driver = input$outputType)
-      }
-      if(input$outputType == 'KML'){
-        writeOGR(obj = shape2(),
-                 dsn = choose.dir(),
-                 layer = filename,
+                 #driver='ESRI Shapefile')
                  driver = input$outputType)
       }
+      # if(input$outputType == 'KML'){
+      #   writeOGR(obj = run(),
+      #            dsn = choose.dir(),
+      #            layer = filename,
+      #            driver = input$outputType)
+      # }
     }
   )
 
--- inst/myshinyapp/ui.r
+++ inst/myshinyapp/ui.r
@@ -2,43 +2,30 @@ shinyUI(fluidPage(
   
   titlePanel("Sample Draw"),
 
-  column(4,
-    selectInput("type", h3("Sampling Type"), choices = list("Equiprobable", "Stratified", "Variable"), selected = 1)
-  ),
-  
-  column(4,
-    selectInput("method", h3("Sampling Method"), choices = list("HAL", "BAS", "GRTS", "SSS"), selected = 1)
-  ),
-  
-  column(4,
-  numericInput("n", "Sample Size", 10, min = 1, max = 1000, step = 10)),
-
-   
- 
-  
-  sidebarLayout(
-  sidebarPanel(
-    fileInput("shape", "Upload Shapefile:", 
+  shinydashboard::box(wellPanel(
+    fluidRow(h3('Please select desired sampling options below.')),
+    fluidRow(selectInput("type", h4("Sampling Type"), choices = list("Equiprobable", "Stratified", "Variable"), selected = 1, width = '80%')),
+    selectInput("method", h4("Sampling Method"), choices = list("HAL", "BAS", "GRTS", "SSS"), selected = 1),
+    numericInput("n", h4("Sample Size"), 10, min = 1, max = 1000, step = 10),
+    fileInput("shape", h4("Upload Shapefile:"), 
               accept = c('.shp', '.dbf', '.sbn', '.sbx', '.shx', '.prj'), multiple = TRUE),
-     selectInput(inputId = 'outputType', label = 'Export File Type:',
-                 choices = c('CSV'='CSV', 'SHP'='ESRI Shapefile', 'KML'='KML')),
-   width = 4
-    ),
+    fluidRow(h3('Please preview your shapefile to the right.'))
+  ), width = 4),
+
+  shinydashboard::box(
+      wellPanel(
+        plotOutput("shape"),
+        actionButton("Run", label = "Take sample", icon = NULL, width = NULL), 
+        width = 4, height =2)
+  ),
   
-  mainPanel(
-    "View Shapefile", 
-    fluidRow(plotOutput("shape")),
-    fluidRow(textOutput('text')),
-    fluidRow(plotOutput("shape2")), 
-    width = 12
-  )),
-    
-  
-  sidebarPanel(
-    actionButton("Run", label = "Run", icon = NULL, width = NULL), 
-    downloadButton("Export", label = "Export"), 
-    actionButton("Quit", label = "Quit", icon = NULL, width = NULL)
-    ), 
+  shinydashboard::box( wellPanel(
+        selectInput(inputId = 'outputType', label = 'Export File Type:',
+                choices = c('CSV'='CSV', 'SHP'='ESRI Shapefile', 'KML'='KML')),
+        downloadButton("Export", label = "Export"),
+        actionButton("Quit", label = "Quit", icon = NULL), 
+        width = 4))
+  ,
   
   theme = "journal.css"))
 
