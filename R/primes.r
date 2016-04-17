102e1cd8339be0c79ec400d89c3a5b08b15c5ac5
tmcd <tmcdonald@west-inc.com>
2016-04-17 12:55:26 -0600

    Changes to primes function
    Exported and included documentation

--- R/primes.r
+++ R/primes.r
@@ -1,8 +1,33 @@
+#' @export primes
+#' 
+#' @title Prime numbers
+#' 
+#' @description Returns the first \code{n} prime numbers (starting at 2)
+#' 
+#' @param n Number prime numbers requested.  Maximum is 1e8 or 100,000,000.
+#' 
+#' @details This routine is brute-force and works well for the low primes, i.e., 
+#' for \code{n} less than a couple thousand.  It is not particularly 
+#' efficient for large \code{n}. 
+#' 
+#' @return A vector of length \code{n} containing prime numbers, in order, 
+#' starting at 2.  
+#' 
+#' @author Trent McDonald
+#' 
+#' @examples 
+#' primes(4) # result is c(2,3,5,7)    
+#'
+#' # Prime pairs in the first 100
+#' p <- primes(100)
+#' p.diff <- diff(p)
+#' cbind(p[-length(p)][p.diff==2], p[-1][p.diff==2])
+
 primes = function(n)
 {
 
   isprime<- function(v){
-    return(sapply(v,function(z){sum(z/1:z==z%/%1:z)})==2)
+    return(sapply(v,function(z){sum((z/1:z) %% 1)==2}))
   }
   
   # return the first n primes
