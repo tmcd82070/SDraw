de7cf1dd28492d3f34fa56b5efc2490c93eae49b
tmcd <tmcdonald@west-inc.com>
2016-04-17 12:32:41 -0600

    Changes to polygonArea
    Exported polygonArea, and completed documentation

--- R/polygonArea.r
+++ R/polygonArea.r
@@ -1,7 +1,35 @@
+#' @export polygonArea
+#' 
+#' @title Polygon area
+#' 
+#' @description An all-R routine that computes area of all 
+#' polygons in a \code{SpatialPolygons*} object, 
+#' taking account of holes.
+#' 
+#' @param x A spatial object inheriting from \code{SpatialPolygons}
+#' 
+#' @details Provides the same answer as \code{rgeos::gArea}, but is 
+#' all-R (does not require rgeos Java library) and does not 
+#' fire a warning if \code{x} is un-projected (i.e., lat-long).
+#' 
+#' @return Area of all polygons in \code{x}, taking account of holes. 
+#' Units of area are squared units of coordinates in \code{x}.  E.g., 
+#' square meters if coordinates in \code{x} are UTM meters, square
+#' decimal degrees if coordinates in \code{x} are lat-long decimial 
+#' degrees.
+#' 
+#' @author Trent McDonald
+#' 
+#' @seealso \code{sp::SpatialPolygons-class}
+#' 
+#' @examples 
+#' 
+#' # Area of Washington state, in hectares
+#' a <- polygonArea( WA ) / (100*100)
+#' 
+
 polygonArea <- function(x){
-  # Compute area of union of all polygons, Taking account 
-  # of holes.  And don't throw warnings if x is unprojected
-  
+
   holes <- sapply(x@polygons, function(xx){sapply(xx@Polygons,slot,"hole")})
   areas <- sapply(x@polygons, function(xx){sapply(xx@Polygons,slot,"area")})
 
