b4e6c94ce33ffdb2f4bfce02a152357e3060809b
tmcd <tmcdonald@west-inc.com>
2016-06-06 12:10:39 -0600

    Package passes CHECK
    But, plot.samp and sdraw documentation generates warnings

--- R/voronoi.polygons.r
+++ R/voronoi.polygons.r
@@ -1,3 +1,5 @@
+#' @export voronoi.polygons
+#' 
 #' @title Calculate Voronoi polygons for a set of points
 #'  
 #' @description Calculate Voronoi polygons (or tessellations) from a 
@@ -34,26 +36,25 @@
 #' @examples 
 #' 
 #'# Triangular grid inside a set of polygons
-#'WA.samp <- sss.polygon(WA,100,triangular=T) 
+#'WA.samp <- sss.polygon(WA,100,triangular=TRUE) 
 #'
 #'# Voronoi polygons of triangular grid 
 #'WA.tess <- voronoi.polygons(WA.samp)
 #'
 #'# Plot 
 #'plot(WA)
-#'plot(WA.tess, add=T, col=rainbow(length(WA.samp)))
-#'plot(WA.samp, add=T, pch=16)
+#'plot(WA.tess, add=TRUE, col=rainbow(length(WA.samp)))
+#'plot(WA.samp, add=TRUE, pch=16)
 #'
 #'# One way to measure spatial balance: 
 #'# Compare variance of Voronoi polygons to same sized 
 #'# SRS sample.  
 #'WA.bas <- bas.polygon(WA, 100)
 #'WA.srs <- srs.polygon(WA, 100)
-# WA.bas.tess <- voronoi.polygons(WA.bas)
-# WA.srs.tess <- voronoi.polygons(WA.srs)
+#'WA.bas.tess <- voronoi.polygons(WA.bas)
+#'WA.srs.tess <- voronoi.polygons(WA.srs)
 #'rel.balance <- var(WA.bas.tess$area)/var(WA.srs.tess$area)
 #'
-#' @export
 
 
 voronoi.polygons <- function(x, bounding.polygon) {
