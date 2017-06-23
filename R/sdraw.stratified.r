2cce80f9e4e2ebaea75427b8cb529ed04cfc5b1c
Aidan McDonald <amcdonald@WEST-INC.LOCAL>
2017-06-23 16:37:30 -0600

    Stratified sampling functionality for points and lines + stratification by specified variable

--- R/sdraw.stratified.r
+++ R/sdraw.stratified.r
@@ -1,12 +1,8 @@
 sdraw.stratified <- function(x, n, type = "BAS", stratified.by = NULL, ...){
   
-  if(!is.null(stratified.by)){
-    strat.col.vals <- eval(parse(text = paste0("x@data$", stratified.by)))
+  if(class(x) != "SpatialPolygonsDataFrame" & is.null(stratified.by)){
+    stop("stratified.by is required for SpatialPoints or SpatialLines")
   }
-  print(strat.col.vals)
-  
-  strata <- list()
-  # SORT ROWS INTO STRATA BASED ON ENTRIES IN STRAT.COL.VARS
   
   if(length(n) == 1){
     n <- rep(n, length(x))
@@ -15,15 +11,42 @@ sdraw.stratified <- function(x, n, type = "BAS", stratified.by = NULL, ...){
     stop("Invalid input for n")
   }
   
+  if(!is.null(stratified.by)){
+    strat.vals <- eval(parse(text = paste0("x@data$", stratified.by)))
+    
+    # Sort rows into strata based on entries in strat.vals
+    unique.strat.vals <- unique(strat.vals)
+    strata <- vector("list", length = length(unique.strat.vals))
+    for(i in 1:length(unique.strat.vals)){
+      for(j in 1:length(strat.vals)){
+        if(strat.vals[j] == unique.strat.vals[i]){
+          if(is.null(strata[[i]])){
+            strata[[i]] <- x[j,]
+          }else{
+            strata[[i]] <- rbind(strata[[i]], x[j,])
+          }
+        }
+      }
+    }
+  }
+  else{
+    for(j in 1:length(x)){
+      strata <- vector("list", length = length(x))
+      strata[[j]] <- x[j,]
+    }
+  }
+  
+  
+  
   # Run sdraw function (with stratified = F) length(x) times
   raw.samples <- list()
-  for(i in 1:length(x)){
-    raw.samples[[i]] <- sdraw(x[i,], n[i], type, stratified = F, ...)
+  for(i in 1:length(strata)){
+    raw.samples[[i]] <- sdraw(strata[[i]], n[i], type, stratified = F, ...)
   }
   
   # Merge the length(x) individual SpatialPointsDataFrames into 1 SpatialPointsDataFrame
   ans <- raw.samples[[1]]
-  for(i in 2:length(x)){
+  for(i in 2:length(strata)){
     ans <- rbind(ans, raw.samples[[i]])
   }
   ans
