e64229d046f744fb5c723efb2af65708c712b4fd
Aidan McDonald <amcdonald@WEST-INC.LOCAL>
2017-06-27 15:32:46 -0600

    Option to input number of points per strata as a data frame

--- R/sdraw.stratified.r
+++ R/sdraw.stratified.r
@@ -1,18 +1,40 @@
 sdraw.stratified <- function(x, n, type = "BAS", stratified.by = NULL, ...){
   
-  if(class(x) != "SpatialPolygonsDataFrame" & is.null(stratified.by)){
-    stop("stratified.by is required for SpatialPoints or SpatialLines")
+  if((class(x) != "SpatialPolygonsDataFrame" | is.data.frame(n)) & is.null(stratified.by)){
+    stop("stratified.by is required for SpatialPoints or SpatialLines or if n is a data frame")
   }
   
-  if(length(n) == 1){
-    n <- rep(n, length(x))
+  if(!is.data.frame(n)){
+    if(length(n) == 1){
+      n <- rep(n, length(x))
+    }
+    else if(!(length(n) == length(x))){
+      stop("Invalid input for n")
+    }
   }
-  else if(!(length(n) == length(x))){
-    stop("Invalid input for n")
-  }
-  
+    
   if(!is.null(stratified.by)){
     strat.vals <- eval(parse(text = paste0("x@data$", stratified.by)))
+    
+    # Rearrange into the correct order if n is a data frame
+    if(is.data.frame(n)){
+      if(!(any(colnames(n) == "n") & any(colnames(n) == stratified.by))){
+        stop(paste("n must have columns named 'n' and", stratified.by))
+      }
+      n.names <- eval(parse(text = paste0("n$", stratified.by)))
+      
+      new.n <- c()
+      for(i in 1:nrow(n)){
+        for(j in 1:length(x))
+        {
+          if(n.names[i] == strat.vals[j]){
+            new.n[j] <- n$n[i]
+            break
+          }
+        }
+      }
+      n <- new.n
+    }
     
     # Sort rows into strata based on entries in strat.vals
     unique.strat.vals <- unique(strat.vals)
