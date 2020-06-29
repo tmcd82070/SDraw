02d59b05c0702fcd05d842802607492473df8d6a
Steven Lan <jlan@west-inc.com>
2020-06-29 16:19:58 -0600

    Added and modified test-sdraw.SpatialPolygons.R (100%/52.21%)

---
+++ tests/testthat/test-sdraw.SpatialPolygons.R
@@ -0,0 +1,31 @@
+##Create spatial polygons object
+##Make "squares" with integer values rounded in a list
+square <- rbind( c(2, 4, 3, 4, 3, 5,
+                   2, 5, 2, 4, 2, 4),
+                 c(6, 9, 7, 9, 7, 8,
+                   6, 8, 6, 9, 6, 9))
+##Give these "squares" an identification
+ID <- c("shape1", "shape2")
+
+##Create SpatialPolygon object from these squares 
+spatPoly <- SpatialPolygons(list(
+  Polygons(list(Polygon(matrix(square[1, ], ncol = 2, byrow = TRUE))), ID[1]),
+  Polygons(list(Polygon(matrix(square[2, ], ncol = 2, byrow = TRUE))), ID[2])
+))
+
+
+context("Test the sdraw.SpatialPolygons function")
+
+test_that("sdraw.SpatialPolygons() operates appropriately", {
+  
+  expect_error(sdraw.SpatialPolygons(spatPoly, 15, type="HOP"),"Invalid SpatialPolygons sample type = HOP",fixed=TRUE )
+  expect_error(sdraw.SpatialPolygons(spatPoly, 20, type="BTS"),"Invalid SpatialPolygons sample type = BTS",fixed=TRUE )
+  expect_error(sdraw.SpatialPolygons(spatPoly, 25, type="SAS"),"Invalid SpatialPolygons sample type = SAS",fixed=TRUE )
+  expect_error(sdraw.SpatialPolygons(spatPoly, 30, type="STS"),"Invalid SpatialPolygons sample type = STS",fixed=TRUE )
+  expect_error(sdraw.SpatialPolygons(spatPoly, 35, type="RTS"),"Invalid SpatialPolygons sample type = RTS",fixed=TRUE )
+  
+  expect_length(sdraw.SpatialPolygons(spatPoly,10, type ="GRTS"),length(primes(10)))
+  
+})
+
+
