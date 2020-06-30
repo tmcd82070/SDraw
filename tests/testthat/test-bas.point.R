b42aeb3d99bad10dd0c16313393b308bf91a0122
Trent <tmcdonald@west-inc.com>
2020-06-30 10:30:49 -0600

    Update test-base.point.R

--- tests/testthat/test-bas.point.R
+++ tests/testthat/test-bas.point.R
@@ -1,4 +1,4 @@
-context("Test the bas.point function")
+context("bas.point")
 
 ##Load pre-built dataset
 data(meuse)
@@ -14,38 +14,40 @@ spdf <- SpatialPointsDataFrame(coords = coords,
                                data = data, 
                                proj4string = crs)
 
-test_that("bas.point() operates appropriately", {
-  
+test_that("no warnings", {
   expect_silent(testSamp<-bas.point(spdf,20))
-  expect_is(testSamp, "SpatialPointsDataFrame")
-  
-  ##These tests look at some individual facets of the base point function
-  ##This is to ensure that the baseline function has operated as it should
-  expect_equal(bas.point(spdf,20)$sampleID, rep(1:20))
-  expect_equal(ncol(bas.point(spdf,20)), 14)
-  expect_equal(nrow(bas.point(spdf,22)), 22)
+})
 
-  ##Now to check for specific column names within this spdf function
-  expect_true("zinc" %in% names(bas.point(spdf,20)))
-  expect_true("lead" %in% names(bas.point(spdf,20)))
-  ##There should be no empty headers, test this below
-  expect_false(" " %in% names(bas.point(spdf, 20)))
+test_that("returns SpatialPointsDataFrame", {
+  expect_is(testSamp, "SpatialPointsDataFrame")
+})
+
+test_that("return contains sampleID", {
+  expect_equal(testSamp$sampleID, rep(1:20))
+})
+
+test_that("return contains geometryID", {
+  expect_true("geometryID" %in% names(testSamp))
+})
+
+test_that("returns correct number of columns", {
+  expect_equal(ncol(testSamp), 14)
+})
+
+test_that("returns correct number of rows", {
+  expect_equal(nrow(testSamp), 20)
+})
+
+test_that("attributes are returned", {
+  expect_true("zinc" %in% names(testSamp))
+  expect_true("lead" %in% names(testSamp))
+})
+
+test_that("no empty header values", {
+  expect_false(" " %in% names(testSamp))
+})
   
-  ##Check every single column name, ensure proper generation
-  answer= names(bas.point(spdf,10))
-  expect_identical(names(bas.point(spdf,10)), answer)
-  
-  ##Use grepl function to check for exact matches
-  answer1 = grepl("ffreq",names(bas.point(spdf,10)))
-  answer2 = grepl("landuse",names(bas.point(spdf,10)))
-  
-  expect_identical(grepl("ffreq",names(bas.point(spdf,10))), answer1)
-  expect_identical(grepl("landuse",names(bas.point(spdf,10))), answer2)
-  
-  
-  
-  ##To ensure that this function will not accept anything other than a SLDF object
-  
+test_that("Throws error on non-SpatialPoints imput", {  
   expect_error(bas.point(8,50), "Must call bas.point with a SpatialPoints* object.", fixed=TRUE)
 })
 
