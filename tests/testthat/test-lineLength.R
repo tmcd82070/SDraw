17bca411cb1dee2f6d3825ab62f0ad71f68edb4a
Steven Lan <jlan@west-inc.com>
2020-07-01 09:59:38 -0600

    Added test-lineLength.R (100%/66.41%)

---
+++ tests/testthat/test-lineLength.R
@@ -0,0 +1,55 @@
+##Create spatial points object
+##Load pre-built dataset
+data(meuse)
+
+##Prepare the coordinates
+
+coords <- meuse[ , c("x", "y")]
+
+##Assign
+spObj <- SpatialPoints(coords)
+
+##Create spatial polygons object
+##Make "squares" with integer values rounded in a list
+square <- rbind( c(2, 4, 3, 4, 3, 5,
+                   2, 5, 2, 4, 2, 4),
+                 c(6, 9, 7, 9, 7, 8,
+                   6, 8, 6, 9, 6, 9))
+##Give these "squares" an identification
+ID <- c("shape1", "shape2")
+
+##Create SpatialPolygon object from these squares 
+spatPoly <- SpatialPolygons(list(
+  Polygons(list(Polygon(matrix(square[1, ], ncol = 2, byrow = TRUE))), ID[1]),
+  Polygons(list(Polygon(matrix(square[2, ], ncol = 2, byrow = TRUE))), ID[2])
+))
+
+##Create some arbitrary lines
+l1 <- cbind(c(1, 2, 3), c(3, 2, 2))
+l2 <- cbind(c(1, 2, 3), c(1, 1.5, 1))
+
+##Creat some formal class lines with these lines
+Sl1 <- Line(l1)
+Sl2 <- Line(l2)
+
+##Assign arbitrary ID's to these formal class lines
+S1 <- Lines(list(Sl1), ID = "a")
+S2 <- Lines(list(Sl2), ID = "b")
+
+##Create a formal class SpatialLines object
+Sl <- SpatialLines(list(S1,S2))
+
+
+context("Test the lineLength function")
+
+
+test_that("lineLength() operates appropriately", {
+  
+  expect_equal(lineLength(spObj), 0)
+  
+  expect_equal(lineLength(spatPoly), length(primes(8)))
+  
+  expect_equal(round(lineLength(Sl),6), 4.650282)
+  
+  
+})
