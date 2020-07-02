546d0facd9d791bebbca4b66d59c5b921fdfd0f8
Steven Lan <jlan@west-inc.com>
2020-07-02 13:21:25 -0600

    Modified test-sss.point and test-srs.point (100%/100%/73.04%)

--- tests/testthat/test-srs.point.R
+++ tests/testthat/test-srs.point.R
@@ -2,13 +2,19 @@
 ##Load pre-built dataset
 data(meuse)
 
-##Prepare the coordinates
+# prepare the 3 components: coordinates, data, and proj4string
 
-coords <- meuse[ , c("x", "y")]
+coords <- meuse[ , c("x", "y")]   # coordinates
+data   <- meuse[ , 3:14]          # data
+crs    <- CRS("+init=epsg:28992") # proj4string of coords
 
 ##Assign
 spObj <- SpatialPoints(coords)
 
+# make the spatial points data frame object
+spdf <- SpatialPointsDataFrame(coords = coords,
+                               data = data, 
+                               proj4string = crs)
 
 context("Test the srs.point function")
 
@@ -16,11 +22,11 @@ context("Test the srs.point function")
 test_that("srs.point() operates appropriately", {
   
   # check if the function stops with message
-  expect_error(obj <- srs.point(1, 1), "Must call srs.point with a SpatialPoints* or data.frame object.",  fixed=TRUE)
+  expect_error(srs.point(1, 1), "Must call srs.point with a SpatialPoints* or data.frame object.",  fixed=TRUE)
   
   # check the output and length
-  expect_named(srs.point(spObj, 5), c("sampleID","geometryID"))
-  expect_identical((srs.point(spObj, 10)$sampleID), 1:10)
+  expect_length(srs.point(coords, 15), length(primes(3)))
+  expect_is(srs.point(spdf, 5), "SpatialPointsDataFrame")
   
   # check input parameters
   expect_length(srs.point(spObj,200), 155)
--- tests/testthat/test-sss.point.R
+++ tests/testthat/test-sss.point.R
@@ -2,12 +2,19 @@
 ##Load pre-built dataset
 data(meuse)
 
-##Prepare the coordinates
+# prepare the 3 components: coordinates, data, and proj4string
 
-coords <- meuse[ , c("x", "y")]
+coords <- meuse[ , c("x", "y")]   # coordinates
+data   <- meuse[ , 3:14]          # data
+crs    <- CRS("+init=epsg:28992") # proj4string of coords
 
 ##Assign
 spObj <- SpatialPoints(coords)
+
+# make the spatial points data frame object
+spdf <- SpatialPointsDataFrame(coords = coords,
+                               data = data, 
+                               proj4string = crs)
 
 
 context("Test the sss.point function")
@@ -19,11 +26,10 @@ test_that("sss.point() operates appropriately", {
   expect_error(obj <- sss.point(1, 1), "Must call sss.point with a SpatialPoints* or data.frame object.",  fixed=TRUE)
   
   # check the output and length
-  expect_named(sss.point(spObj, 5), c("sampleID","geometryID"))
-  expect_identical((sss.point(spObj, 10)$sampleID), 1:10)
+  expect_length(sss.point(coords, 15), length(primes(3)))
+  expect_is(sss.point(spdf, 5), "SpatialPointsDataFrame")
   
   # check input parameters
   expect_length(sss.point(spObj,200), 155)
   expect_equal(sss.point(spObj,0), NULL)
-  
 })
