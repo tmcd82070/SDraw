48da8a10375bf5fcce9851c2955123d51ae7a72c
Steven Lan <jlan@west-inc.com>
2020-07-02 11:43:01 -0600

    Modified test-sss.line and test-srs.line (100%/100%/72.53%)

--- tests/testthat/test-srs.line.R
+++ tests/testthat/test-srs.line.R
@@ -1,4 +1,9 @@
+library(rgeos)
 ##Taken from the vignette of the sp package:
+
+##This section of code serves the purpose of creating a "mock"
+##SLDF in an effort to properly test the baseline function in a stand-alone
+##manner.
 
 ##Create some arbitrary lines
 l1 <- cbind(c(1, 2, 3), c(3, 2, 2))
@@ -15,6 +20,13 @@ S2 <- Lines(list(Sl2), ID = "b")
 ##Create a formal class SpatialLines object
 Sl <- SpatialLines(list(S1,S2))
 
+##Create a fake dataframe for our new SLDF object
+df <- data.frame(len = sapply(1:length(Sl), function(i) gLength(Sl[i, ])))
+rownames(df) <- sapply(1:length(Sl), function(i) Sl@lines[[i]]@ID)
+
+
+##SpatialLines to SpatialLinesDataFrame
+Sldf <- SpatialLinesDataFrame(Sl, data = df)
 
 context("Test the srs.line function")
 
@@ -22,9 +34,13 @@ test_that("srs.line() operates appropriately", {
   
   # check if x is a SpatialLines* object
   expect_error(obj <- srs.line(1,5), "Must call srs.line with a SpatialLines* object.",fixed=TRUE)
+  expect_is(srs.line(Sl,10), "SpatialPointsDataFrame")
+  expect_is(Sldf,"SpatialLinesDataFrame")
+  expect_type(Sldf, "S4")
   
   # check output and length
   expect_equal(length(srs.line(Sl,26)), length(letters))
+  expect_length(srs.line(Sldf,5), length(primes(5)))
   
   # check input parameters
   expect_equal(srs.line(Sl,0),NULL)
--- tests/testthat/test-sss.line.R
+++ tests/testthat/test-sss.line.R
@@ -1,3 +1,4 @@
+library(rgeos)
 ##Taken from the vignette of the sp package:
 
 ##Create some arbitrary lines
@@ -15,6 +16,13 @@ S2 <- Lines(list(Sl2), ID = "b")
 ##Create a formal class SpatialLines object
 Sl <- SpatialLines(list(S1,S2))
 
+##Create a fake dataframe for our new SLDF object
+df <- data.frame(len = sapply(1:length(Sl), function(i) gLength(Sl[i, ])))
+rownames(df) <- sapply(1:length(Sl), function(i) Sl@lines[[i]]@ID)
+
+##SpatialLines to SpatialLinesDataFrame
+Sldf <- SpatialLinesDataFrame(Sl, data = df)
+
 
 context("Test the sss.line function")
 
@@ -22,11 +30,15 @@ test_that("sss.line() operates appropriately", {
   
   # check if x is a SpatialLines* object
   expect_error(obj <- sss.line(1,1), "Must call sss.line with a SpatialLines* object.",fixed=TRUE)
+  expect_is(sss.line(Sl,10), "SpatialPointsDataFrame")
+  expect_is(Sldf,"SpatialLinesDataFrame")
+  expect_type(Sldf, "S4")
   
   #check if n or spacing is missing with message
   expect_warning(sss.line(Sl,10,10),"n and spacing both specified in sss.line.  n is being used.")
   
   ## check input parameters
   expect_equal(sss.line(Sl,0),NULL)
+  expect_length(sss.line(Sldf,10, random.start = FALSE), length(primes(10)))
   
 })
