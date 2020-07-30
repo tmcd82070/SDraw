3632521b0c0d7a38922cab18852e64752a5452c2
Steven Lan <jlan@west-inc.com>
2020-07-30 09:42:14 -0600

    Modified test file

--- tests/testthat/test-hip.point.R
+++ tests/testthat/test-hip.point.R
@@ -11,6 +11,7 @@ coords <- meuse[ , c("x", "y")]
 
 # assign
 spObj <- SpatialPoints(coords)
+spObj2x <- SpatialPoints(rbind(coords, coords))
 spObj6 <- spObj[1:6,]
 
 # check if the function stops with message
@@ -20,7 +21,7 @@ test_that("n must be a SpatialPoints* object", {
 
 test_that("check the output and length",{
   expect_named(hip.point(spObj, 5), "sampleID")
-  expect_identical((hip.point(spObj, 10,  plot.lattice = TRUE)$sampleID), 1:10)
+  expect_identical((hip.point(spObj2x, 10, J=c(4,2), plot.lattice = TRUE)$sampleID), 1:10)
 })
 
 test_that("n will be assigned to 1 if n is less than 1",{
@@ -31,3 +32,13 @@ test_that("n is greater than length of sample frame", {
   expect_warning(hip.point(spObj6,10), "Sample size is greater than points in the sample frame. 
             n has been reset to the total number of points (i.e., drawing a census)." ,fixed=TRUE)
 })
+
+test_that("J results in more Halton boxes than there are points", {
+  expect_warning(hip.point(spObj, 15, J=c(3,5)), "J results in more Halton boxes than there are points. J has been set to default value.", fixed=TRUE)
+})
+
+# test_that("", {
+#   expect_warning(hip.point(spObj2x, 300, J=c(4,2)), c("Sample size is greater than max sample size for HIP sampling.
+#   HIP sampling discards some points while drawing Halton lattice.
+#   n has been set to ", maxSampleSize, ", the largest possible sample for N = ", N, " and J = (", J[1], ",", J[2], ")"), ,fixed=TRUE)
+# })
