9f4765d4367c78055354b961026e0837fc0ce8eb
Steven Lan <jlan@west-inc.com>
2020-08-16 14:50:18 -0600

    Modified test-halton.indices.R

--- tests/testthat/test-halton.indices.R
+++ tests/testthat/test-halton.indices.R
@@ -16,11 +16,6 @@ crs    <- CRS("+init=epsg:28992") # proj4string of coords
 spObj <- SpatialPoints(coords)
 bb <- bbox(WA.cities) + c(0,0,1,1)
 
-# make the spatial points data frame object
-spdf <- SpatialPointsDataFrame(coords = coords,
-                               data = data, 
-                               proj4string = crs)
-
 # assign cases
 # x is SpatialPoints, missing hl.bbox
 case_1 <- halton.indices(spObj, J=c(3,2))
@@ -28,8 +23,6 @@ case_1 <- halton.indices(spObj, J=c(3,2))
 # x is a data frame, J is null, missing hlbox, use.CRT=TRUE
 case_2 <- halton.indices(coords, use.CRT=TRUE)
 
-# x is SpatialPointsDataFrame
-case_3 <- halton.indices(spdf)
 
 # the first run always succeeds, but warns
 # subsequent runs will suceed only if the file is unchanged
@@ -42,9 +35,5 @@ test_that("halton.indices(case_1) returns equivalent obj as it did previously", 
 # # x is DataFrame, missing hl.bbox
 test_that("halton.indices(case_2) returns equivalent obj as it did previously", {
   expect_known_value(case_2, "halton.indices_case_2.rds")
-})
-
-test_that("halton.indices(case_3) returns equivalent obj as it did previously", {
-  expect_known_value(case_3, "halton.indices_case_3.rds")
 })
 
