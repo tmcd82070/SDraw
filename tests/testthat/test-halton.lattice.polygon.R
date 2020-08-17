f63ec8c57013f919848af9e9e619679ee86b85dd
Steven Lan <jlan@west-inc.com>
2020-08-17 10:47:08 -0600

    Modified test-halton.lattice.polygon.R

--- tests/testthat/test-halton.lattice.polygon.R
+++ tests/testthat/test-halton.lattice.polygon.R
@@ -25,10 +25,6 @@ spatPoly <- SpatialPolygons(list(
 # the first run always succeeds, but warns
 # subsequent runs will suceed only if the file is unchanged
 # this will fail the first time if the output changes
-# test_that("halton.lattice.polygon() returns equivalent obj as it did previously", {
-#   expect_known_value(halton.lattice.polygon(WY, eta=c(3,2), triangular=TRUE), "halton.lattice.polygon.rds")
-# })
-
 # x is not SpatialPolygonsDataFrame
 test_that("halton.lattice.polygon(spatPoly) returns equivalent obj as it did previously", {
   expect_known_value(halton.lattice.polygon(spatPoly), "halton.lattice.polygon_notSPDF.rds")
